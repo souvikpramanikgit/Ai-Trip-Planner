83b844142d9087ced7c505a544c0cfcce21d2d05
souvikpramanikgit <souvikpramanik874@gmail.com>
2024-09-24 03:11:19 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,40 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import CreateTrip from './components/create-trip/index.jsx'
+import Header from './components/custom/Header.jsx'
+import ViewTrip from './view-trip/[tripid]/index.jsx'
+import MyTrips from './my-trips/index.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import { Toaster } from './components/ui/sonner.jsx'
+
+const router = createBrowserRouter([
+  {
+  path:'/',
+  element:<App/>
+},
+{
+  path:'/create-trip',
+  element:<CreateTrip/>
+},
+{
+  path:'/view-trip/:tripId',
+  element:<ViewTrip/>
+},
+{
+  path:'/my-trips',
+  element:<MyTrips/>
+}
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
+      <Header/>
+      <Toaster />
+      <RouterProvider router={router}/>
+    </GoogleOAuthProvider>
+  </StrictMode>,
+)
